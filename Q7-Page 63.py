b4d9025ce528ed905dc699c099f0a321c4449359
AmirNouri1 <39634652+AmirNouri1@users.noreply.github.com>
2022-02-28 00:09:46 +0330

    Add files via upload

---
+++ Q7-Page 63.py
@@ -0,0 +1,134 @@
+i1 = 2
+i2 = 5
+i3 = -3
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+
+a = i1 + i2
+print("(a)", end=" ")
+print(a)
+print(type(a))
+
+
+b = i1 / i2
+print("(b)", end=" ")
+print(b)
+print(type(b))
+
+
+c = i1 // i2
+print("(c)", end=" ")
+print(c)
+print(type(c))
+
+
+d = i2 / i1
+print("(d)", end=" ")
+print(d)
+print(type(d))
+
+
+e = i1 / i2 + i3
+print("(e)", end=" ")
+print(e)
+print(type(e))
+
+
+f = i1 * i3
+print("(f)", end=" ")
+print(f)
+print(type(f))
+
+
+g = d1 + d2
+print("(g)", end=" ")
+print(g)
+print(type(g))
+
+
+h = d1 / d2
+print("(h)", end=" ")
+print(h)
+print(type(h))
+
+
+i = d2 / d1
+print("(i)", end=" ")
+print(i)
+print(type(i))
+
+
+j = d3 * d1
+print("(j)", end=" ")
+print(j)
+print(type(j))
+
+
+k = d1 + i2
+print("(k)", end=" ")
+print(k)
+print(type(j))
+
+
+l = i1 / d2
+print("(l)", end=" ")
+print(l)
+print(type(l))
+
+
+m = d2 / i1
+print("(m)", end=" ")
+print(m)
+print(type(m))
+
+
+n = i2 / d1
+print("(n)", end=" ")
+print(n)
+print(type(n))
+
+
+o = i1 / i2 * d1
+print("(o)", end=" ")
+print(o)
+print(type(o))
+
+
+p = d1 * i1 / i2
+print("(p)", end=" ")
+print(p)
+print(type(p))
+
+
+q = d1 / d2 * i1
+print("(q)", end=" ")
+print(q)
+print(type(q))
+
+
+r = i1 * d1 / d2
+print("(r)", end=" ")
+print(r)
+print(type(r))
+
+s = i2 / i1 * d1
+print("(s)", end=" ")
+print(s)
+print(type(s))
+
+t = d1 * i2 / i1
+print("(t)", end=" ")
+print(t)
+print(type(t))
+
+u = d2 / d1 * i1
+print ("(u)", end=" ")
+print(u)
+print(type(u))
+
+v = i1 * d2 / d1
+print ("(v)", end=" ")
+print(v)
+print(type(v))
+
